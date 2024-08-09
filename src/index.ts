728ea24f5cd8c1091c7ded655fbfeefedc14d0f8
GaoNeng-wWw <gaonenggg@outlook.com>
2024-08-09 21:42:51 +0800

    fix: if router not exists will throw 404

--- src/index.ts
+++ src/index.ts
@@ -152,6 +152,10 @@ export async function createMockServer(opt: Partial<BootstarpOption>){
     }
     const methods = req.method?.toLowerCase() ?? 'get';
     const route = router[pathname][methods];
+    if (!router){
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(''); 
+    }
     if (!route.response){
       res.writeHead(404, { 'Content-Type': 'application/json' });
       res.end('');
